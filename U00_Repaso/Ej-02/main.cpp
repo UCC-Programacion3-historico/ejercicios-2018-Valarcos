7b2985674595835de242fe387e8d55a9d430df20
Valarcos <marcosachavalmbaj@gmail.com>
2019-08-07 11:01:34 -0300

    Empezamos a resolver ejercicios de la materia, revisar en compu propia y actualizar los archivos como haga falta

--- U00_Repaso/Ej-02/main.cpp
+++ U00_Repaso/Ej-02/main.cpp
@@ -1,9 +1,13 @@
 #include <iostream>
 #include "Calculadora.h"
+#include <vector>
 
 using namespace std;
 
 int main() {
+    Calculadora<int, float> miCalc;
+
+    cout << miCalc.divi(10, 3);
     cout << "Ejercicio 00/02\n" << endl;
     return 0;
 }
